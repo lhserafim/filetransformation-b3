b395dd00ffab6a7e09586e058c5ea9d8391e2300
lhserafim <lhserafim@gmail.com>
2023-10-27 21:47:23 -0300

    Download - WIP

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+q
+Asrc/main/java/com/suhai/transformab3/TransformaB3Application.java,1/2/12fbb738466454d20cc7e4bd06db324bb33b4ec6
+s
+Csrc/main/java/com/suhai/transformab3/controller/FileController.java,f/f/ff5143740e1e102934fa827bd9a1cfb491c50510
--- src/main/java/com/suhai/transformab3/controller/FileController.java
+++ src/main/java/com/suhai/transformab3/controller/FileController.java
@@ -1,13 +1,21 @@
 package com.suhai.transformab3.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
 
@@ -15,6 +23,8 @@ import java.util.Arrays;
 @Controller
 @RequestMapping(value = "b3/file")
 public class FileController {
+
+    private static final String UPLOAD_DIR = "/Users/lhserafim/Downloads/B3/source/21403_SEG_230815_SP_MOVIMENTO-DIARIO-AUTOMOVEL-TXT.txt";
 
 
     @PostMapping(value = "/transformation")
@@ -24,5 +34,24 @@ public class FileController {
         return "file-list";
     }
 
+    @GetMapping("/download")
+    public ResponseEntity<InputStreamResource> downloadFile() {
+        try {
+            File file = new File(UPLOAD_DIR );
+            FileInputStream inputStream = new FileInputStream(file);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "attachment; filename=xxxx.txt" );
+
+            return ResponseEntity
+                    .ok()
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(new InputStreamResource(inputStream));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
